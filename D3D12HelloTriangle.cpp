da64291f5e8b87bfeb9b511c0c21570497434bcb
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-12-26 16:31:38 +0900

    create raytracing resources

--- D3D12HelloTriangle.cpp
+++ D3D12HelloTriangle.cpp
@@ -48,6 +48,14 @@ void D3D12HelloTriangle::OnInit()
     // rays (ray payload)
     CreateRaytracingPipeline(); // #DXR
 
+    // Allocate the buffer storing the raytracing output, with the same dimensions
+    // as the target image
+    CreateRaytracingOutputBuffer(); // #DXR
+
+    // Create the buffer containing the raytracing result (always output in a
+    // UAV), and create the heap referencing the resources used by the raytracing,
+    // such as the acceleration structure
+    CreateShaderResourceHeap(); // #DXR
 }
 
 // Load the rendering pipeline dependencies.
@@ -676,33 +684,27 @@ void D3D12HelloTriangle::CreateRaytracingOutputBuffer() {
 //
 void D3D12HelloTriangle::CreateShaderResourceHeap() {
     // Create a SRV/UAV/CBV descriptor heap. We need 2 entries - 1 UAV for the
-    // raytracing output and 1 SRV for the TLAS
-    m_srvUavHeap = nv_helpers_dx12::CreateDescriptorHeap(
-        m_device.Get(), 2, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, true);
+    // raytracing output and 1 SRV for the TLAS (Corresponds to RayGen.hlsl)
+    m_srvUavHeap = nv_helpers_dx12::CreateDescriptorHeap(m_device.Get(), 2, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, true);
 
-    // Get a handle to the heap memory on the CPU side, to be able to write the
-    // descriptors directly
-    D3D12_CPU_DESCRIPTOR_HANDLE srvHandle =
-        m_srvUavHeap->GetCPUDescriptorHandleForHeapStart();
+    // Get a handle to the heap memory on the CPU side, to be able to write the descriptors directly
+    D3D12_CPU_DESCRIPTOR_HANDLE srvHandle = m_srvUavHeap->GetCPUDescriptorHandleForHeapStart();
 
     // Create the UAV. Based on the root signature we created it is the first
-    // entry. The Create*View methods write the view information directly into
-    // srvHandle
+    // entry. The Create*View methods write the view information directly into srvHandle
     D3D12_UNORDERED_ACCESS_VIEW_DESC uavDesc = {};
     uavDesc.ViewDimension = D3D12_UAV_DIMENSION_TEXTURE2D;
-    m_device->CreateUnorderedAccessView(m_outputResource.Get(), nullptr, &uavDesc,
-        srvHandle);
+    m_device->CreateUnorderedAccessView(m_outputResource.Get(), nullptr, &uavDesc,srvHandle);
 
     // Add the Top Level AS SRV right after the raytracing output buffer
-    srvHandle.ptr += m_device->GetDescriptorHandleIncrementSize(
-        D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+    srvHandle.ptr += m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
 
     D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc;
     srvDesc.Format = DXGI_FORMAT_UNKNOWN;
     srvDesc.ViewDimension = D3D12_SRV_DIMENSION_RAYTRACING_ACCELERATION_STRUCTURE;
     srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
-    srvDesc.RaytracingAccelerationStructure.Location =
-        m_topLevelASBuffers.pResult->GetGPUVirtualAddress();
+    srvDesc.RaytracingAccelerationStructure.Location = m_topLevelASBuffers.pResult->GetGPUVirtualAddress();
+
     // Write the acceleration structure view in the heap
     m_device->CreateShaderResourceView(nullptr, &srvDesc, srvHandle);
 }
